72a5ab10ee10a010a5a87bc558ff9bd7bb9f2f20
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-01-08 08:29:41 +0000

    Add tests to check recsim

--- system_tests/tests/litron.py
+++ system_tests/tests/litron.py
@@ -3,7 +3,7 @@ import unittest
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from utils.testing import get_running_lewis_and_ioc
 
 
 DEVICE_PREFIX = "LITRON_01"
@@ -13,22 +13,35 @@ IOCS = [
     {
         "name": DEVICE_PREFIX,
         "directory": get_default_ioc_dir("LITRON"),
-        "macros": {},
+        "macros": {"IPADDR": "127.0.0.1", "VI_PATH": "C:/instrument/dev/ibex_vis/"},
         "emulator": "Litron",
     },
 ]
 
 
-TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+TEST_MODES = [TestModes.RECSIM]
 
 
 class LitronTests(unittest.TestCase):
     """
     Tests for the Litron IOC.
     """
+
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("Litron", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def test_THAT_nudge_changes_distance(self):
+        self.ca.assert_setting_setpoint_sets_readback(
+            1, "OPO:CRYS:NUDGE:DIST", "OPO:CRYS:NUDGE:DIST:SP"
+        )
+
+    def test_THAT_nudge_up_changes(self):
+        self.ca.assert_setting_setpoint_sets_readback(
+            1, "OPO:CRYS:NUDGE:UP", "OPO:CRYS:NUDGE:UP:SP"
+        )
+
+    def test_THAT_nudge_down_changes_down(self):
+        self.ca.assert_setting_setpoint_sets_readback(
+            1, "OPO:CRYS:NUDGE:DN", "OPO:CRYS:NUDGE:DN:SP"
+        )
