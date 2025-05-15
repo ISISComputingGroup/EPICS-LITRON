717659310334a5411da77ef8c13f63ed776813d4
LowriJenkins <40766142+LowriJenkins@users.noreply.github.com>
2025-05-15 11:02:14 +0100

    Reflect review changes to DB

--- system_tests/tests/litron.py
+++ system_tests/tests/litron.py
@@ -98,25 +98,25 @@ class LitronTests(unittest.TestCase):
         
         
         # Make sure the buffer is empty.
-        self.ca.set_pv_value("STALE_CHECK:BUFFER.RES", 1)
-        self.ca.set_pv_value("STALE_CHECK:BUFFER:MAX.RES", 1)
-        self.ca.set_pv_value("STALE_CHECK:BUFFER:MIN.RES", 1)
+        self.ca.set_pv_value("_STALE_CHECK:BUFFER.RES", 1)
+        self.ca.set_pv_value("_STALE_CHECK:BUFFER:MAX.RES", 1)
+        self.ca.set_pv_value("_STALE_CHECK:BUFFER:MIN.RES", 1)
         
         
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER", empty_buffer)
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER:MAX", 0)
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER:MIN", 0)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER", empty_buffer)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER:MAX", 0)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER:MIN", 0)
         self.ca.assert_that_pv_is("STALE", "Stale")
         
         self.ca.set_pv_value("SIM:WL", 1)
         
         
         
-        self.ca.assert_that_pv_value_causes_func_to_return_true("STALE_CHECK:BUFFER", _check_empty_and_1)
+        self.ca.assert_that_pv_value_causes_func_to_return_true("_STALE_CHECK:BUFFER", _check_empty_and_1)
         self.ca.set_pv_value("SIM:WL", -1)
-        self.ca.assert_that_pv_value_causes_func_to_return_true("STALE_CHECK:BUFFER", _check_empty_and_both)
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER:MAX", 1)
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER:MIN", -1)
+        self.ca.assert_that_pv_value_causes_func_to_return_true("_STALE_CHECK:BUFFER", _check_empty_and_both)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER:MAX", 1)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER:MIN", -1)
         self.ca.assert_that_pv_is("STALE", "Good")
         
     
@@ -126,12 +126,12 @@ class LitronTests(unittest.TestCase):
         
         # Buffer not stale when noise is enabled (default) on emulator
         self.ca.assert_that_pv_is("STALE", "Good")
-        self.ca.assert_that_pv_is_not("STALE_CHECK:BUFFER", [0.0] * STALE_TIME)
+        self.ca.assert_that_pv_is_not("_STALE_CHECK:BUFFER", [0.0] * STALE_TIME)
         
         # Buffer becomes stale after STALE_TIME has passed seconds after hardware disconnect
         self._lewis.backdoor_set_on_device("hardware_connected", False)
         self.ca.assert_that_pv_is("WL", 0)
-        self.ca.assert_that_pv_is("STALE_CHECK:BUFFER", empty_buffer, timeout=25)
+        self.ca.assert_that_pv_is("_STALE_CHECK:BUFFER", empty_buffer, timeout=25)
         self.ca.assert_that_pv_is("STALE", "Stale", timeout=STALE_TIME)
         self.ca.assert_that_pv_alarm_is("STALE", self.ca.Alarms.MAJOR, timeout=STALE_TIME)
 
